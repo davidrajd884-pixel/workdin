691cbd166346bed4ccf75c5534669333c5faa39a
davidrajd884-pixel <davidrajd884@gmail.com>
2026-06-23 12:25:42 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,7 +8,7 @@ getDocs
 } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
 const firebaseConfig = {
-apiKey: "AIzaSyDIpwCvxlpzIiWv6hoDaHSFCRJ7X41-gGU",
+apiKey: "YOUR_API_KEY",
 authDomain: "workdin-a654f.firebaseapp.com",
 projectId: "workdin-a654f",
 storageBucket: "workdin-a654f.firebasestorage.app",
@@ -19,7 +19,9 @@ appId: "1:938476466822:web:a6e143bd24bb13b106d707"
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-window.db = db;
-window.collection = collection;
-window.addDoc = addDoc;
-window.getDocs = getDocs;
+export {
+db,
+collection,
+addDoc,
+getDocs
+};
